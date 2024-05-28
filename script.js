2d7a22c42f7d5d07422d87656e8d9436594d50e6
Dragomir Zlatanov <dzlatanov13@gmail.com>
2024-05-28 08:58:49 +0300

    Having issues with calc after first calculation

--- script.js
+++ script.js
@@ -16,31 +16,56 @@ console.log(add(5, 6),
     divide(5, 6),
     divide(110, 0));
 console.log(1);
-let valueOne;
-let valueTwo;
-let operator;
+let valueOne = '';
+let valueTwo = '';
+let operator = '';
 let display = document.getElementById("value");
+let result = '';
 function operate(valueOne, valueTwo, operate){
     switch (operate){
         case '+':
-            add(valueOne, valueTwo);
-            break;
+            return add(valueOne, valueTwo);
         case '-':
-            subtract(valueOne, valueTwo);
-            break;
+            return subtract(valueOne, valueTwo);
         case '/':
-            divide(valueOne, valueTwo);
-            break;
+            return divide(valueOne, valueTwo);
         case '*':
-            multiply(valueOne, valueTwo);
-            break;
+            return multiply(valueOne, valueTwo);
         default:
             alert("Operator is wrong!");
     }
 }
 
 function updateDisplay(value) {
-    display.textContent += value ;
+    let boolForDisplay = display.textContent==="/"||display.textContent==="+"||display.textContent==="*"||display.textContent==="-";
+    let boolForValue = value==='+'||value==='-'||value==='*'||value==='/';
+    let boolForNumberValue = value==='0'||value==='1'||value==='2'||value==='3'||value==='4'||value==='5'||value==='6'||value==='7'||value==='8'||value==='9';
+    if(boolForNumberValue && !boolForDisplay){
+        display.textContent += value;
+    } else if(boolForNumberValue && boolForDisplay && valueOne !== ''){
+        if(boolForDisplay){
+            operator = display.textContent;
+            display.textContent = value;
+            console.log(operator);
+        } else {
+            display.textContent += value;
+        }
+    } else if(boolForValue && boolForDisplay){
+        display.textContent = value;
+    } else if(boolForValue && valueOne==='') {
+        valueOne = Number(display.textContent);
+        display.textContent = value;
+        console.log(valueOne);
+    }
+    if (value==='='){
+        valueTwo = Number(display.textContent);
+        result = operate(valueOne, valueTwo, operator)
+        display.textContent = result;
+        valueOne = result;
+        valueTwo = '';
+        operator = '';
+    }
 
 }
+
 
